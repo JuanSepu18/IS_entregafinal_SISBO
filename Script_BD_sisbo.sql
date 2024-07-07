e958a2bb80111cbe7fd6c91b59a4f7aed122086d
Carlos <ceguito06@gmail.com>
2024-07-07 15:15:35 -0500

    añadiendo datos, faltaron boletas y servicios adicionales

--- Script_BD_sisbo.sql
+++ Script_BD_sisbo.sql
@@ -28,31 +28,31 @@ CREATE TABLE IF NOT EXISTS `boleta` (
   KEY `id_seguidor` (`id_seguidor`),
   CONSTRAINT `boleta_ibfk_2` FOREIGN KEY (`id_localidad`) REFERENCES `localidad` (`id_localidad`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `boleta_ibfk_4` FOREIGN KEY (`id_seguidor`) REFERENCES `seguidor` (`documento_de_identidad`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- Volcando datos para la tabla sisbo.boleta: ~3 rows (aproximadamente)
+-- Volcando datos para la tabla sisbo.boleta: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `boleta` DISABLE KEYS */;
-INSERT INTO `boleta` (`id_boleta`, `precio`, `mercado_secundario`, `id_localidad`, `id_seguidor`) VALUES
-	(1, 250000, 0, 1, '111111111111111'),
-	(2, 250000, 0, 1, NULL),
-	(3, 250000, 1, 1, NULL);
 /*!40000 ALTER TABLE `boleta` ENABLE KEYS */;
 
 -- Volcando estructura para tabla sisbo.club
 CREATE TABLE IF NOT EXISTS `club` (
   `id_club` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(300) UNIQUE COLLATE utf8_spanish2_ci NOT NULL,
-  `correo_electronico` varchar(200) UNIQUE COLLATE utf8_spanish2_ci NOT NULL,
+  `nombre` varchar(300) COLLATE utf8_spanish2_ci NOT NULL,
+  `correo_electronico` varchar(200) COLLATE utf8_spanish2_ci NOT NULL,
   `contrasena` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
   `estadio_propio` varchar(300) COLLATE utf8_spanish2_ci NOT NULL,
-  PRIMARY KEY (`id_club`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+  PRIMARY KEY (`id_club`),
+  UNIQUE KEY `nombre` (`nombre`),
+  UNIQUE KEY `correo_electronico` (`correo_electronico`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- Volcando datos para la tabla sisbo.club: ~2 rows (aproximadamente)
+-- Volcando datos para la tabla sisbo.club: ~4 rows (aproximadamente)
 /*!40000 ALTER TABLE `club` DISABLE KEYS */;
 INSERT INTO `club` (`id_club`, `nombre`, `correo_electronico`, `contrasena`, `estadio_propio`) VALUES
-	(1, 'Atletico bucaramanga', 'AtleticoBga@gmail.com', 'bucaramanga123', 'Americo montanini'),
-	(4, 'Atletico Nacional', 'AtleticoNacional@gmail.com', 'nacional123', 'asdasd');
+	(6, 'Atletico Bucaramanga', 'AtleticoBga@gmail.com', 'bucaramanga123', 'Americo Montanini'),
+	(7, 'Millonarios FC', 'millonarios@gmail.com', 'millonarios123', 'El campin'),
+	(8, 'Atletico Nacional', 'nacional@gmail.com', 'nacional123', 'Atanasio Girardot'),
+	(9, 'Junior de Barranquilla', 'junior@gmail.com', 'junior123', 'Metropolitano Roberto Meléndez');
 /*!40000 ALTER TABLE `club` ENABLE KEYS */;
 
 -- Volcando estructura para tabla sisbo.club_correovinculado
@@ -79,12 +79,17 @@ CREATE TABLE IF NOT EXISTS `eventodeportivo` (
   PRIMARY KEY (`id_evento`),
   KEY `id_club` (`id_club`),
   CONSTRAINT `eventodeportivo_ibfk_1` FOREIGN KEY (`id_club`) REFERENCES `club` (`id_club`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- Volcando datos para la tabla sisbo.eventodeportivo: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla sisbo.eventodeportivo: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `eventodeportivo` DISABLE KEYS */;
 INSERT INTO `eventodeportivo` (`id_evento`, `oponente`, `estadio`, `fecha`, `hora_ingreso`, `hora_cierre`, `id_club`) VALUES
-	(1, 'Millonarios FC', 'Americo Montanini', '2024-07-27', '19:00', '23:00', 1);
+	(13, 'Atletico Bucaramanga', 'Metropolitano Roberto Meléndez', '2024-07-13', '15:00', '18:00', 9),
+	(14, 'Millonarios FC', 'El campin', '2024-07-27', '18:00', '21:00', 9),
+	(15, 'Atletico Nacional', 'El campin', '2024-07-13', '19:00', '22:00', 7),
+	(16, 'Deportivo Pereira', 'Hernán Ramírez Villegas', '2024-08-10', '13:00', '16:00', 7),
+	(17, 'Junior de Barranquilla', 'Americo Montanini', '2024-07-27', '17:00', '20:00', 6),
+	(18, 'Atletico Nacional', 'El campin', '2024-08-10', '15:00', '18:00', 6);
 /*!40000 ALTER TABLE `eventodeportivo` ENABLE KEYS */;
 
 -- Volcando estructura para tabla sisbo.localidad
@@ -98,29 +103,53 @@ CREATE TABLE IF NOT EXISTS `localidad` (
   PRIMARY KEY (`id_localidad`),
   KEY `id_evento` (`id_evento`),
   CONSTRAINT `localidad_ibfk_1` FOREIGN KEY (`id_evento`) REFERENCES `eventodeportivo` (`id_evento`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- Volcando datos para la tabla sisbo.localidad: ~2 rows (aproximadamente)
+-- Volcando datos para la tabla sisbo.localidad: ~8 rows (aproximadamente)
 /*!40000 ALTER TABLE `localidad` DISABLE KEYS */;
 INSERT INTO `localidad` (`id_localidad`, `nombre`, `precio`, `cantidad_puestos_total`, `cantidad_puestos_vendidos`, `id_evento`) VALUES
-	(1, 'Sur', 250000, 1000, 200, 1),
-	(2, 'Norte', 200000, 1000, 900, 1);
+	(12, 'Occidental', 200000, 5000, 0, 13),
+	(13, 'Sur', 350000, 2000, 0, 13),
+	(14, 'Oriental', 380000, 5000, 0, 13),
+	(15, 'Norte', 350000, 2000, 0, 13),
+	(16, 'Norte', 400000, 2000, 0, 14),
+	(17, 'Oriental', 450000, 5000, 0, 14),
+	(18, 'Occidental', 400000, 6000, 0, 14),
+	(19, 'Sur', 350000, 2000, 0, 14),
+	(20, 'Occidental', 450000, 6000, 0, 15),
+	(21, 'Norte', 400000, 2000, 0, 15),
+	(22, 'Oriental', 350000, 5000, 0, 15),
+	(23, 'Sur', 350000, 2000, 0, 15),
+	(24, 'Norte', 250000, 1000, 0, 16),
+	(25, 'Occidental', 300000, 3000, 0, 16),
+	(26, 'Oriental', 320000, 2800, 0, 16),
+	(27, 'Sur', 250000, 1000, 0, 16),
+	(28, 'Occidental', 250000, 10000, 0, 17),
+	(29, 'Sur', 350000, 3800, 0, 17),
+	(30, 'Norte', 300000, 4000, 0, 17),
+	(31, 'Oriental', 250000, 9000, 0, 17),
+	(32, 'Norte', 400000, 5000, 0, 18),
+	(33, 'Sur', 400000, 5000, 0, 18),
+	(34, 'Occidental', 300000, 8000, 0, 18),
+	(35, 'Oriental', 320000, 9000, 0, 18);
 /*!40000 ALTER TABLE `localidad` ENABLE KEYS */;
 
 -- Volcando estructura para tabla sisbo.seguidor
 CREATE TABLE IF NOT EXISTS `seguidor` (
   `documento_de_identidad` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
   `nombre` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
-  `correo_electronico` varchar(255) UNIQUE COLLATE utf8_spanish2_ci NOT NULL,
+  `correo_electronico` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
   `contrasena` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
-  PRIMARY KEY (`documento_de_identidad`)
+  PRIMARY KEY (`documento_de_identidad`),
+  UNIQUE KEY `documento_de_identidad` (`documento_de_identidad`),
+  UNIQUE KEY `correo_electronico` (`correo_electronico`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- Volcando datos para la tabla sisbo.seguidor: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `seguidor` DISABLE KEYS */;
 INSERT INTO `seguidor` (`documento_de_identidad`, `nombre`, `correo_electronico`, `contrasena`) VALUES
-	('111111111111111', 'Juan Sepulveda', 'Juan123@gmail.com', 'juan123'),
-	('1234567890', 'Carlos Garcia', 'Carlos123@gmail.com', 'carlos1234');
+	('1111111111', 'Juan Sepulveda', 'juan@gmail.com', 'juan123'),
+	('9999999999', 'Carlos Garcia', 'carlos@gmail.com', 'carlos123');
 /*!40000 ALTER TABLE `seguidor` ENABLE KEYS */;
 
 -- Volcando estructura para tabla sisbo.servicioadicionalclub
@@ -135,13 +164,10 @@ CREATE TABLE IF NOT EXISTS `servicioadicionalclub` (
   PRIMARY KEY (`id_servicio_club`),
   KEY `id_club` (`id_club`),
   CONSTRAINT `servicioadicionalclub_ibfk_1` FOREIGN KEY (`id_club`) REFERENCES `club` (`id_club`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
--- Volcando datos para la tabla sisbo.servicioadicionalclub: ~2 rows (aproximadamente)
+-- Volcando datos para la tabla sisbo.servicioadicionalclub: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `servicioadicionalclub` DISABLE KEYS */;
-INSERT INTO `servicioadicionalclub` (`id_servicio_club`, `nombre`, `descripcion`, `precio`, `unidades_totales`, `unidades_vendidas`, `id_club`) VALUES
-	(6, 'Camiseta oficial', 'Esta es la camisa amarilla con verde', 150000, 500, 100, 1),
-	(7, 'Gorra oficial', 'Esta es la gorra amarilla con el escudo', 40000, 500, 100, 1);
 /*!40000 ALTER TABLE `servicioadicionalclub` ENABLE KEYS */;
 
 -- Volcando estructura para tabla sisbo.servicioadicionalseguidor
@@ -153,12 +179,10 @@ CREATE TABLE IF NOT EXISTS `servicioadicionalseguidor` (
   PRIMARY KEY (`id_servicio_seguidor`),
   KEY `id_seguidor` (`id_seguidor`),
   CONSTRAINT `servicioadicionalseguidor_ibfk_1` FOREIGN KEY (`id_seguidor`) REFERENCES `seguidor` (`documento_de_identidad`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- Volcando datos para la tabla sisbo.servicioadicionalseguidor: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `servicioadicionalseguidor` DISABLE KEYS */;
-INSERT INTO `servicioadicionalseguidor` (`id_servicio_seguidor`, `nombre`, `descripcion`, `id_seguidor`) VALUES
-	(1, 'Camiseta Oficial', 'La camisa del Bucaramanga', '111111111111111');
 /*!40000 ALTER TABLE `servicioadicionalseguidor` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
